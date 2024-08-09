dd80e2967b874f4b96da998dfcbd9705e29c2b63
kemila <kemiladabare4@gmail.com>
2024-08-09 23:40:01 +0530

    adding coffe data

---
+++ lib/data/coffee_items_data.dart
@@ -0,0 +1,75 @@
+class CoffeeDataClass {
+  static List<Map<String, dynamic>> coffeeData = [
+  {
+    "id": "1",
+    "name": "Espresso",
+    "description": "A strong and bold coffee shot with rich flavor and aroma.",
+    "price": "2.50",
+    "imageUrl" :"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Tazzina_di_caff%C3%A8_a_Ventimiglia.jpg/1200px-Tazzina_di_caff%C3%A8_a_Ventimiglia.jpg",
+  },
+  {
+    "id": "2",
+    "name": "Cappuccino",
+    "description": "A classic Italian coffee with steamed milk foam and a hint of cocoa.",
+    "price": "3.00",
+    "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Cappuccino_at_Sightglass_Coffee.jpg/1200px-Cappuccino_at_Sightglass_Coffee.jpg",
+  },
+  {
+    "id": "3",
+    "name": "Latte",
+    "description": "A smooth and creamy coffee with steamed milk and a light layer of foam.",
+    "price": "3.50",
+    "imageUrl": "https://www.foodandwine.com/thmb/CCe2JUHfjCQ44L0YTbCu97ukUzA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Partners-Latte-FT-BLOG0523-09569880de524fe487831d95184495cc.jpg",
+  },
+  {
+    "id": "4",
+    "name": "Americano",
+    "description": "A rich and robust coffee made by diluting espresso with hot water.",
+    "price": "2.75",
+    "imageUrl": "https://www.foodandwine.com/thmb/9JyfZPcxlV9ubEeuSznhO-M4q0w=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Partners-Americano-FT-BLOG0523-b8e18cc340574cc9bed536cceeec7082.jpg",
+  },
+  {
+    "id": "5",
+    "name": "Mocha",
+    "description": "A chocolate-flavored coffee with steamed milk and whipped cream.",
+    "price": "4.00",
+    "imageUrl": "https://athome.starbucks.com/sites/default/files/2021-06/1_CAH_CaffeMocha_Hdr_2880x16602.jpg",
+  },
+  {
+    "id": "6",
+    "name": "Macchiato",
+    "description": "An espresso with a small amount of foamed milk, offering a bold flavor.",
+    "price": "3.25",
+    "imageUrl": "https://parade.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTk1MzA0NTgyOTIwODA3NzM4/what-is-a-macchiato.jpg",
+  },
+  {
+    "id": "7",
+    "name": "Flat White",
+    "description": "A smooth and velvety coffee with microfoam, offering a creamy texture.",
+    "price": "3.75",
+    "imageUrl": "https://www.foodandwine.com/thmb/xQZv2CX6FO5331PYK7uGPF1we9Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Partners-Flat-White-FT-BLOG0523-b11f6273c2d84462954c2163d6a1076d.jpg",
+  },
+  {
+    "id": "8",
+    "name": "Iced Coffee",
+    "description": "A refreshing cold coffee served over ice with a choice of milk or sweeteners.",
+    "price": "3.00",
+    "imageUrl": "https://frostingandfettuccine.com/wp-content/uploads/2022/12/Caramel-Iced-Coffee-6.jpg",
+  },
+  {
+    "id": "9",
+    "name": "Affogato",
+    "description": "A delicious coffee-based dessert with espresso poured over vanilla ice cream.",
+    "price": "4.50",
+    "imageUrl": "https://static01.nyt.com/images/2021/08/15/magazine/affogato/affogato-superJumbo-v2.jpg",
+  },
+  {
+    "id": "10",
+    "name": "Irish Coffee",
+    "description": "A warm and boozy coffee with Irish whiskey, sugar, and a layer of cream.",
+    "price": "5.00",
+    "imageUrl": "https://static01.nyt.com/images/2018/03/02/dining/irish-coffee/irish-coffee-superJumbo.jpg",
+  },
+];
+
+}
